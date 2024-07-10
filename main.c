ae94fa186fd2abda9dae895a52303ff5b6de90c4
hwzoe <johyewon102@gamil.com>
2024-07-10 17:05:20 +0900

    hw1-2

--- main.c
+++ main.c
@@ -15,6 +15,10 @@
 
 // Simulation of the CPU hardware sleeping mode
 // Idle task hook, 지우면 안됨
+
+//Task handle 이름 선언
+TaskHandle_t task3;
+
 void vApplicationIdleHook( void )
 {
     usleep( 15000 );
@@ -40,16 +44,30 @@ void vTask2( void *pvParameters )
 	{
 		console_print( pcTaskName );
         vTaskDelay( 5000 );
+		vTaskResume(task3);
+	}
+}
+// Task 3 정의
+void vTask3(void *pvParameters)
+{
+	const char*pcTaskName = "5분만...\r\n";
+	for(;;)
+	{
+		console_print( pcTaskName );
+		vTaskSuspend(task3);
 	}
 }
 
+
 int main( void )
 {
+
     console_init(); 
 
 	xTaskCreate( vTask1, "Task 1", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-	xTaskCreate( vTask2, "Task 2", configMINIMAL_STACK_SIZE, NULL, 2, NULL );
-    
+	xTaskCreate( vTask2, "Task 2", configMINIMAL_STACK_SIZE, NULL, 3, NULL );
+    xTaskCreate( vTask3, "Task 3", configMINIMAL_STACK_SIZE, NULL, 2, &task3 );
+
 	vTaskStartScheduler();
 	for( ;; );
 }
